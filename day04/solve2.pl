877e074e493ae57d03d696702b3ede4b7742b321
TheOtherDoctor <rockingrooster@hotmail.com>
2022-12-04 23:37:53 +0100

    code modification for 2nd star of day 4.

---
+++ day04/solve2.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl
+# AoC2022 day 04 - camp cleanup - puzzle star 1 + 2
+# count the pairs of region overlaps.
+use strict;
+use warnings;
+
+# return 1 when point is within range,
+# with r0 being smaller than r1.
+sub ptInRange{
+  my ($p,$r0,$r1)=@_;
+  return ( ($p>=$r0) && ($p<=$r1) );
+}
+
+# counter:
+my $numembed=0; # fully covered ranges
+my $numendover=0; # only one-sided overlapping ranges
+# loop over file
+open(FH,"input") or die "filefail.\n";
+while(<FH>){
+  chomp();
+  print "$_ --> ";
+  my ($a0,$a1,$b0,$b1);
+  # all-in-one regex match:
+  if(m/(\d+)-(\d+),(\d+)-(\d+)/){ # expected format
+    ($a0,$a1,$b0,$b1)=($1,$2,$3,$4); # alias matches
+    # full coverage == boths b's in range a or both a's in range b:
+    my $embeds=0; # flag (both conds can apply, do not count twice...)
+    if( (ptInRange($b0, $a0,$a1) && ptInRange($b1, $a0,$a1)) ){ $embeds=1; print "left covers right. "; }
+    if( (ptInRange($a0, $b0,$b1) && ptInRange($a1, $b0,$b1)) ){ $embeds=1; print "right covers left. "; }
+    if($embeds==1){ # disjoint counters
+      $numembed+=$embeds;
+    # one-sided overlap == one point from a is inside range b 
+    }elsif(ptInRange($a0, $b0,$b1) || ptInRange($a1, $b0,$b1)) { 
+      $numendover++; 
+      print "single-side overlap";
+    }
+  }else{ # no regex match:
+    die "table format oops.";
+  }
+  print "\n";
+  # silently assuming that format is always "min(a)-max(a),min(b)-max(b)"...
+  # and never like "max(a)-min(a),..."
+} 
+close(FH);
+print "overall count of fully embedded ranges in pairs = $numembed.\n";
+print "overall count of somehow-overlapping ranges in pairs = ".($numendover+$numembed).".\n";
