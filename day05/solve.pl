64988143601897510ffc6a2f834c9899c3e580d8
TheOtherDoctor <rockingrooster@hotmail.com>
2022-12-06 00:46:36 +0100

    working code for star 1 of day 5i, with logs for testinput and input.

--- day05/solve.pl
+++ day05/solve.pl
@@ -6,7 +6,10 @@ use strict;
 use warnings;
 use Data::Dumper; # for cheap prettyprint checks.
 
+my $INFILE="input";
+
 # create the initial stacks from the top part of the input:
+# takes given stack as call arg.
 sub dummyinit{ # for getting started without annoying read:
   # example wants enumeration from 1, will not do that with
   # data struct, will adapter that in call transparently then.
@@ -19,7 +22,39 @@ sub dummyinit{ # for getting started without annoying read:
   # using c'tor brackets, see "Make rule 2" from "man perlreftut".
 }
 
-sub readinit{ # sooner or later gotta do that annoying reader. later. 
+# read initial stack setup from file:
+# note that there are trailing spaces to make all lines full length.
+# takes given stack as call arg, and filename from global,
+# so same call as dummy above.
+sub readinit{ 
+  my ($sp) = @_; # stack pointer
+  open(FH,"$INFILE") or die "file fail";
+  my $fline=0;
+  while(<FH>){
+    print "read init content line ".$fline++." ...\n";
+    chomp();
+    if(m/^ 1   2/){close(FH); return;}
+
+    # eat this/each line colblock by colblock:
+    my $idx=0; # stacknum to target.
+    while($_){
+      if(s/^(...)//){ # grab/remove first 3 chars, always true
+        my $snip=$1;
+        if($snip=~m/^\[([A-Z])\]/){ # something to push
+          my $char=$1;
+          push(@{$sp->[$idx]},$char); # push it... operating on top
+        }
+      }else{ die "logic error."; }
+      # if there is something left, advance a field: 
+      if($_){ 
+        s/^ //; # remove the separator space 
+        $idx++; # and incr. index
+      }
+    } # (while content left in line)
+    
+  } # (while lines in file)
+  die "failed to find init header end in input.";
+  # no good end here.
 }
 
 # operate on two given stacks:
@@ -32,16 +67,28 @@ sub movecrates{
   }
 }
 
+# write the string of top items as report:
+sub report{
+  my($stacks)=@_;
+  my $report="";
+  foreach my $s (@{$stacks}){
+    my $topitem=shift(@{$s}); 
+    unshift(@{$s},$topitem); # no RO function should leave it crippled. ;-)
+    $report=$report.$topitem;
+  }
+  print "$report\n";
+}
+
 # list of stacks:
 my @stacks;
 # will contain ptrs to lists, so we get a LoL.
 
 # read/generate content:
-dummyinit(\@stacks);
+readinit(\@stacks);
 print Dumper(@stacks)."\n";
 
 # execute the movement commands:
-open(FH,"testinput") or die "file fail";
+open(FH,"$INFILE") or die "file fail";
 while(<FH>){
   # ok, we know there is only 9 stacks, so one digit as of now:
   if(m/move (\d+) from (\d) to (\d)/){
@@ -52,6 +99,9 @@ while(<FH>){
     movecrates($num,$stacks[$from-1],$stacks[$to-1]); 
     # check state:
     print Dumper(@stacks)."\n";
+    # report compressed result:
   }
 }
+print "final report: "; 
+report(\@stacks);
 close(FH);
