e5d5bd950ebfb1de3fb259a62ad30217712220d0
TheOtherDoctor <rockingrooster@hotmail.com>
2022-12-05 23:20:13 +0100

    reproduce the test example hardwired first.

---
+++ day05/solve.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl
+# AoC day 05 star 1 - supply stacks
+
+# implement the elves' cargo stapling 
+use strict;
+use warnings;
+use Data::Dumper; # for cheap prettyprint checks.
+
+# create the initial stacks from the top part of the input:
+sub dummyinit{ # for getting started without annoying read:
+  # example wants enumeration from 1, will not do that with
+  # data struct, will adapter that in call transparently then.
+  my ($sp) = @_; # stack pointer
+  # construct stacks from bottom to top:
+  $sp->[0] = [ qw(Z N) ];
+  $sp->[1] = [ qw(M C D) ];
+  $sp->[2] = [ qw(P) ];
+  # using c'tor brackets, see "Make rule 2" from "man perlreftut".
+}
+
+sub readinit{ # sooner or later gotta do that annoying reader. later. 
+}
+
+# operate on two given stacks:
+sub movecrates{
+  my ($num,$from,$to)=@_;
+  while($num>0){
+    my $e=pop(@{$from});
+    push(@{$to},$e);
+    $num--;
+  }
+}
+
+# list of stacks:
+my @stacks;
+# will contain ptrs to lists, so we get a LoL.
+
+# read/generate content:
+dummyinit(\@stacks);
+print Dumper(@stacks)."\n";
+
+# execute the movement commands:
+open(FH,"testinput") or die "file fail";
+while(<FH>){
+  # ok, we know there is only 9 stacks, so one digit as of now:
+  if(m/move (\d+) from (\d) to (\d)/){
+    my ($num,$from,$to)=($1,$2,$3); # name matches
+    print "moving $num from stack $from to stack $to...\n";
+    # keep helper simple and explicitly hand over the two stacks,
+    # here we also map internal index [0..] to control [1..]:
+    movecrates($num,$stacks[$from-1],$stacks[$to-1]); 
+    # check state:
+    print Dumper(@stacks)."\n";
+  }
+}
+close(FH);
