d7d7040d164172f56a245dca6f8b0c68ec84e6eb
TheOtherDoctor <rockingrooster@hotmail.com>
2022-12-09 01:03:22 +0100

    resignation note for star 2 of today, considered too far from design concept done for star 1. :-(

---
+++ day08/solve2.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+# AoC day 08 star 2 - treetop scene score
+
+# star 2 today is seen as impossible based on design of star 1:
+# I assumed that a single digit is fine to store the (visibility, boolean)
+# information. Now if part 2 will result in numbers >9, that needs a data
+# structure of a real matrix then. 
+# Success seen as too unlikely to try with old structure.
+# Will let go star 2 and do this as a homework, probably in (num)py, later.
+# good night.
+
